2c8c224d30d6ee89a08166c3cb46f748d1864209
Adoxhh <87052113@qq.com>
2018-04-10 16:04:13 +0800

    git学习

---
+++ src/ado/xhh.java
@@ -0,0 +1,19 @@
+package ado;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * 找出数组中第一个重复出现的数
+ */
+public class xhh {
+	public static void main(String[] args) {
+		String a = ",efsdf,";
+		a= a.replaceAll(",","");
+		System.out.println(a);
+	}
+}
