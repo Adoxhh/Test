6202e8992d2d09af778ccde2bab59895eb5eb0ac
nk <nk@DESKTOP-ITVE5I0>
2018-04-17 16:31:19 +0800

    1

--- src/ado/AquickSort.java
+++ src/ado/AquickSort.java
@@ -1,68 +1,49 @@
 package ado;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 /**
  * 快速排序 给定一个数组, 
  * 1:选取数组第一个数作为基准点; 
  * 2:比基准点小的放左边,比基准点大的放右边; 
  * 3:重复步骤2,得到排序数组.
- *
  */
-public class AquickSort { 
-	
-    public static void quickSort(int[] a) {  
-        if(a.length>0) {  
-            quickSort(a, 0 , a.length-1);  
-        }  
-    }  
-    
-    private static void quickSort(int[] a, int low, int high) {
-    	if (low < high) {
-    		int middle = a.length/2;
-			a = quickCore(a);
-			quickSort(a, 0, middle); 
-			quickSort(a, middle+1, high);
-		}		
+public class AquickSort {
+	// 暴露只一个参数的公共接口
+	public static void quickSort(int a[]) {
+		sort(a, 0, a.length - 1);
 	}
-    //获取基准点
-    private static int[] quickCore(int[] a) {
-		//基准点
-		List<Integer> l = new ArrayList<Integer>();
-		List<Integer> r = new ArrayList<Integer>();
-		for (int i = 0; i < a.length; i++) {
-			if(a[i]<=a[0]) {
-				l.add(a[i]);
+
+	// 快排算法的真正实现
+	private static void sort(int[] a, int low, int high) {
+		if (low >= high) {
+			return;
+		}
+		int i = low, j = high; // 设置这两个变量的目的是为了保持low和high不变
+		int pivotNum = a[i]; // 基准数
+		while (i < j) {
+			while (a[j] >= pivotNum && j > i) { // 循环结束的条件有二：一是找到比支点小的数，二是j==i
+				j--;
 			}
-			if(a[i]>a[0]) {
-				r.add(a[i]);
+			if (j > i) { // 由于上面循环结束的功能性有两个，对于找到比支点小的数，即j!=i，要进行位置的交换，下同
+				a[i] = a[j];
+				i++;
+			}
+			while (a[i] < pivotNum && i < j) {
+				i++;
+			}
+			if (i < j) {
+				a[j] = a[i];
+				j--;
 			}
 		}
-		int[] p = new int[a.length];
-		for (int i = 0; i < l.size(); i++) {
-			p[i]=l.get(i);
-		}
-		for (int i = 0; i < r.size(); i++) {
-			p[l.size()+i]=r.get(i);
-		}
-		return p;
+		a[i] = pivotNum;
+		sort(a, low, i - 1);
+		sort(a, i + 1, high);
 	}
-	/**
-	 * 交换
-	 */
-	private static void change(int i, int j) {
-		if(i == j) return;
-		int temp = i;
-		i=j;
-		j=temp;
+
+	public static void main(String[] args) {
+		int[] a = { 52, 39, 67, 95, 70, 8, 25, 52, 0, -1, 1, 1, -3 };
+		quickSort(a);
+		for (int i : a) {
+			System.out.print(i + " ");
+		}
 	}
-	public static void main(String[] args) {  
-//        int[] a = {4, 2, 4, 5, 7, 4, 5 ,3 ,9 ,0,-3,3,1};  
-        int[] a = {2,1,3,2,1,4};  
-        System.out.println(Arrays.toString(a));  
-        quickSort(a,0,a.length);  
-        System.out.println(Arrays.toString(a));  
-    }  
 }
