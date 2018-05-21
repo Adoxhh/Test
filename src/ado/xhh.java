e68ab910ff12aa5eb5ed24adc8930277364302dd
Adoxhh <87052113@qq.com>
2018-05-21 09:51:59 +0800

    1

--- src/ado/xhh.java
+++ src/ado/xhh.java
@@ -1,20 +1,8 @@
 package ado;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class xhh {
+public class xhh{
 	public static void main(String[] args) {
-		List<String> list1 =new ArrayList<String>();
-		list1.add("A");
-		list1.add("B");
-		System.out.println(list1);
-		List<String> list2 =new ArrayList<String>();
-		list2.add("B");
-		list2.add("C");
-		System.out.println(list2);
-		list1.retainAll(list2);
-		
-		System.out.println(list1);
+//		double f = 13.0/30.0;
+		System.out.println(13.0/30.0);
 	}
 }
